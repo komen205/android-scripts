bcd57a7ceebfd44f8b0bc8ae61ffbd032a2b5f79
nuno <59452877+komen205@users.noreply.github.com>
2022-08-30 22:31:38 +0100

    Add files via upload

--- apk_so_extractor.py
+++ apk_so_extractor.py
@@ -1,5 +1,18 @@
 import zipfile
 import os
+import sys
 
-with zipfile.ZipFile("app-release.apk", 'r') as zip_ref:
-    zip_ref.extractall('lib')
+#This script will extract the libraries of an apk
+path = sys.argv[1]
+
+if path is None:
+    print("please input the absolute path of the apk")
+
+
+archive = zipfile.ZipFile(path)
+
+for file in archive.namelist():
+    if file.startswith('lib/'):
+        archive.extract(file, 'extracted_libs')
+
+print("libs were extracted")
